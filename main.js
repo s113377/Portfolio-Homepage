e74cf2daeafe3607c2f414e4b68ded922d9548ef
Inbeom <85912032+s113377@users.noreply.github.com>
2021-08-25 14:25:28 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+$(function(){
+    var tabAnchor = $('.Sub_GNB li');
+    var tabPanel = $('.tabPanel');
+
+    tabAnchor.click(function(e){
+        e.preventDefault();
+       
+        tabAnchor.removeClass('active');
+        $(this).addClass('active');
+
+    tabPanel.hide();
+
+    var $targetIdx = $(this).index();
+    console.log($targetIdx)
+    tabPanel.eq($targetIdx).show();
+    });
+    
+    tabAnchor.eq(0).trigger('click');
+});
+
+$(document).ready(function(){
+    var Btn = $('.tabPanel #Btn');
+    var close = $('.close');
+    var Modal = $('#Modal')
+
+    Btn.click(function(e){
+        e.preventDefault();
+         Modal.stop().show();
+    close.click(function(){
+         Modal.stop().hide();
+    })
+    })
+})
